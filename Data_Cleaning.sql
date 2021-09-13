4383b6955ee7b44ba317ff13b917dff84373bbd3
RSanchez96 <vicsanabanto@gmail.com>
2021-09-13 06:15:24 -0500

    Update Data_Cleaning.sql
    Limpieza de los registros de las BD

--- Data_Cleaning.sql
+++ Data_Cleaning.sql
@@ -14,13 +14,27 @@ WHERE rideable_type = 'classic_bike';
 /*Revisando los nombres de las estaciones */
 
 SELECT DISTINCT star_station_name
-FROM july_2021_cyclistic
+FROM july_2021_cyclistic;
 
 /*Encontre estaciones de prueba entre lo registrado (estas incluian nombres como Test o Base).
 Para no considerar datos falsos, borre estas de los registros mensuales */
 
 DELETE FROM august_2020_cyclistic
-WHERE start_station_name ILIKE '%TEST%' OR start_station_name ILIKE '%BASE%'
+WHERE start_station_name ILIKE '%TEST%' OR start_station_name ILIKE '%BASE%';
 
-/*Encontre estaciones de prueba entre lo registrado (estas incluian nombres como Test o Base).
-Para no considerar datos falsos, borre estas de los registros mensuales */
+/*Revise en las bases de datos en caso haya alguna entrada de tipo de usuario incorrecta, sin encontrar datos falsos */
+
+SELECT DISTINCT (member_casual)
+FROM september_2020_cyclistic;
+
+/*Verificque si habian usos de la bicicleta con una duración menor a un minuto, 
+ya que asumi que eran entradas falsas */
+
+SELECT (ended_at - started_at), start_station_name, end_station_name AS ride_time
+FROM august_2020_cyclistic
+WHERE (ended_at - started_at) < '00:01:00';
+
+/*Elimine estas entradas */
+
+DELETE FROM august_2020_cyclistic
+WHERE (ended_at - started_at) < '00:01:00';
